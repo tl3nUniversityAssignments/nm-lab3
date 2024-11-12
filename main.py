b9a270b6bee158601b65b4d549d902e843ef8aa3
Vasyl Pavlyuchenko <vasyl.pavlyuchenko@gmail.com>
2024-11-12 21:33:20 +0200

    min eigenvalue finding function

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import numpy as np
 import sympy as sp
 
-def get_dummy_matrix():
+def get_dummy_matrix1():
     return np.array([[2, 1, 0],
                      [1, 2, 1],
                      [0, 1, 2]], dtype=float)
+
+def get_dummy_matrix2():
+    return np.array([[2, -1, 0],
+                     [-1, 2, -1],
+                     [0, -1, 2]], dtype=float)
 
 def get_matrix():
     return np.array([[10, 1, 0, 1],
@@ -21,7 +26,7 @@ def norm(x):
 
 # перевірити необхідні умови для мошуку мінімального власного значення
 # знайти максимальне власне значення
-def max_eigenvalue_exp(A, eps = 0.5): # 
+def max_eigenvalue_exp(A, eps = 1e-6): # 
     n = A.shape[0]
     x_prev = np.ones(n).reshape(n, 1)
     x = A @ x_prev
@@ -42,7 +47,18 @@ def max_eigenvalue_exp(A, eps = 0.5): #
         
         l_prev = l
 
-A = get_dummy_matrix()
+def min_eigenvalue_exp(A, max_eigenvalue):
+    n = A.shape[0]
+    E = np.eye(n)
+    B = max_eigenvalue * E - A
+
+    B_max_eigenvalue = max_eigenvalue_exp(B)
+    return max_eigenvalue - B_max_eigenvalue
+
+#A = get_dummy_matrix1()
+A = get_dummy_matrix2()
 #A = get_matrix()
 max_eigenvalue = max_eigenvalue_exp(A)
 print(max_eigenvalue)
+min_eigenvalue = min_eigenvalue_exp(A, max_eigenvalue)
+print(min_eigenvalue)
